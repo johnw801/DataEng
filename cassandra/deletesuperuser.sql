76d1d51c008b9ccd6f860234f115b173a59a7b28
Bazin12213 <ioawal@proton.me>
2025-11-10 16:59:08 +0100

    Delete cassandra/deletesuperuser.sql

--- cassandra/deletesuperuser.sql
+++
@@ -1,5 +0,0 @@
--- Default-Superuser 'cassandra' neutralisieren. BITTE PW ÄNDERN !!!!!!
-ALTER ROLE cassandra
-  WITH PASSWORD = 'initialespasswortbitteaendern'
-  AND SUPERUSER = false
-  AND LOGIN = false;
